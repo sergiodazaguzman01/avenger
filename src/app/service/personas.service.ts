69cd834d8be12e1081ba30e8142bffd73a67ad04
SANTIAGO <santiago.guzman.7406@eam.edu.co>
2022-01-18 21:07:18 -0500

    creado personajess

--- src/app/service/personas.service.ts
+++ src/app/service/personas.service.ts
@@ -68,14 +68,41 @@ export class  PersonaService {
       frase: "Truth is... that this is all my fault. I accidentally brought those dangerous people here. And if those people are watching... just know that I really did try to help you. I mean, I could've killed you. At any given moment, but I didn't. Because my Aunt May taught me that everyone deserves a second chance."
     },
     {
-      img: "assets/img/DoctorStrange.webp",
+      img: "assets/img/CapMarvel.webp",
+      nombre:"Carol Susan Jane Danvers",
+      alias: "Captain Marvel",
+      especie: "Human/Kree Hybrid",
+      nacionalidad: "American Kree Imperial",
+      fechaNacimiento: "Mid-1960s",
+      frase: "I'm coming to end it. The war, the lies, all of it."
+    },
+    {
+      img: "assets/img/BlackPanther.webp",
       nombre: "T'Challa",
-      alias: "Black Panther\n",
+      alias: "Black Panther",
       especie: "Human",
       nacionalidad: "Wakandan",
       fechaNacimiento: "NA",
-      frase: "Truth is... that this is all my fault. I accidentally brought those dangerous people here. And if those people are watching... just know that I really did try to help you. I mean, I could've killed you. At any given moment, but I didn't. Because my Aunt May taught me that everyone deserves a second chance."
+      frase: "My name is King T'Challa, son of King T'Chaka. I am the sovereign ruler of the nation of Wakanda. And for the first time in our history, we will be sharing our knowledge and resources with the outside world."
     },
+    {
+      img: "assets/img/Loki.webp",
+      nombre: "Loki Laufeyson",
+      alias: "Loki",
+      especie: "Frost Giant",
+      nacionalidad: "Asgardian Jotun",
+      fechaNacimiento: "965 A.D.",
+      frase: "I, Loki, Prince of Asgard, Odinson, the rightful King of Jotunheim, God of Mischief, do hereby pledge to you, my undying fidelity."
+    },
+    {
+      img: "assets/img/BlackWindow.webp",
+      nombre: "Natalia Alianovna Romanoff",
+      alias: "Black Widow",
+      especie: "Human",
+      nacionalidad: "Russian",
+      fechaNacimiento: "December 3, 1984",
+      frase: "I used to have nothing. And then I got this. This job. This family. And I was better because of it. And even though they're gone... I'm still trying to be better."
+    }
   ];
 
     constructor() {
